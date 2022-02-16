d79820262dc0ce19965ac955cb4cdc93f2344141
josequesadaGL <jose.quesada@gorillalogic.com>
2022-02-16 08:27:38 -0600

    Moving the UI validations to Spec files
    UI validations where performed implicitly when interacting with components, now validations were moved to the spec files as explicit

--- cypress/support/pages/product.js
+++ cypress/support/pages/product.js
@@ -3,15 +3,6 @@ import locators from '../locators/product'
 
 class ProductPage extends BasePage {
 
-    // *** Validations *** //
-    validateNavigationToProductPage(productSlug) {
-        cy.url().should('include', `/product/${productSlug.toLowerCase()}`)
-        this.validateShopBanner(productSlug)
-    }
-
-    validateShopBanner(productName) {
-        this.getBannerText().should("eq", productName)
-    }
 }
 
 export default new ProductPage()
